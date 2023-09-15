6c6567fe6c275c80a62c89a8932499506e37a171
jbusacca <106988925+jbusacca@users.noreply.github.com>
2023-09-15 14:04:04 -0400

    Add files via upload

--- Wk1_Project/PizzaShop/src/main/java/Application.java
+++ Wk1_Project/PizzaShop/src/main/java/Application.java
@@ -1,8 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args) {
 
+        List<String> menu = new ArrayList<>();
 
+            menu.add("Cheese Pizza");
+            menu.add("Pepperoni Pizza");
+            menu.add("Buffalo Chicken Pizza");
+            menu.add("Vegetarian Pizza");
+
+            Menu
 
     }
 }
